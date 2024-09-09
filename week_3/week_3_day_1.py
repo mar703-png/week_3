b1efd187ff25e21257038c94c36c514a12937d07
Bahena <mbahena39@cps.k12.il.us>
2024-09-09 12:47:43 -0500

    age = 25

--- week_3/week_3_day_1.py
+++ week_3/week_3_day_1.py
@@ -2,12 +2,32 @@
 # # This week we will work on :
 # # Working With Strings
 
-
 # # 1.   Working With Numbers
 # # 2.   Getting Input From Users
 
-
-
-
 # # 1.   Building a Basic Calculator
 # # 2.   Mad Libs Game
+
+# # Review
+# create variables for the following :
+# 1. age
+age = 25
+# 2. name
+name = "John"
+# 3. song
+song = "Happy Birthday"
+# 4. food
+food = "Apples"
+# 5. number
+number = 1000
+
+# #now include the variables you just made print in the following...
+# Once upon a time, there was a [age] old coder named [name].
+#concatenation
+print ("Once upon a time, there was a " + str(age) + " old coder named " + name + ".")
+#f string
+print (f"Once upon a time, there was a {age} old coder named {name}.")
+
+# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+# Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
+##########################################################################################
