fa823d954bc061930816e9fd024644526961bc37
Bahena <mbahena39@cps.k12.il.us>
2024-09-09 12:08:10 -0500

    first commit

---
+++ week_3_day_1_.py
@@ -0,0 +1,13 @@
+# # Week3
+# # This week we will work on :
+# # Working With Strings
+
+
+# # 1.   Working With Numbers
+# # 2.   Getting Input From Users
+
+
+
+
+# # 1.   Building a Basic Calculator
+# # 2.   Mad Libs Game
