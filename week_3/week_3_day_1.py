27f9755de14c98fd1f981e54b91c7c0366285cd3
Bahena <mbahena39@cps.k12.il.us>
2024-09-11 12:20:07 -0500

    valid variables

--- week_3/week_3_day_1.py
+++ week_3/week_3_day_1.py
@@ -31,7 +31,10 @@
 # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
 # Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
 
+
 ##########################################################################################
+
+
 # The names you use when creating these labels need to follow a few rules:
 # 1. Names can not start with a number.
 # 2. There can be no spaces in the name, use _ instead.
@@ -78,7 +81,9 @@
 # percentage
 # i
 
+
 ############################################################################################
+
 
 # # **Working with** **numbers** **bold text**
 # We'll learn about the following topics:
@@ -91,6 +96,7 @@
 # 1. We'll mainly focus on integers and floating point numbers.
 # Integers are just whole numbers, positive or negative. For example: 2 and -2 are examples of integers.
 # 2. Floating point numbers in Python are notable because they have a decimal point in them, or use an exponential (e) to define the number. For example 2.0 and -2.1 are examples of floating point numbers. 4E2 (4 times 10 to the power of 2) is also an example of a floating point number in Python.
+
 
 ##########################################################################################
 # #addition
@@ -134,18 +140,6 @@ print(sqrt(36)) #square root method
 # multiplied by itself gives the number
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 ##########################################################################################
 # So what have we learned? We learned some of the basics of numbers in Python. We also learned how to do arithmetic and use Python as a basic calculator. We then wrapped it up with learning about Variable Assignment in Python.
 # # **Getting Input from users**
@@ -159,12 +153,6 @@ print(sqrt(36)) #square root method
 # # find the max number
 # # find the remainder of the numbers
 # #round one number
-
-
-
-
-
-
 
 
 ##########################################################################################
