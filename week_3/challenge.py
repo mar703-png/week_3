537bffb97b8657337f8f7acf0cbf49f0e9a28a90
Bahena <mbahena39@cps.k12.il.us>
2024-09-13 12:13:09 -0500

    changes made

---
+++ week_3/challenge.py
@@ -0,0 +1,24 @@
+# this is just for testing
+# for collab purposes
+# create an input for the user to input their age
+# create an input for the user to input their name
+# create an input for the user to input their favorite song
+
+# age = input("What is your age?")
+# print("You are,", age)
+
+# name = input("What is your name?")
+# print("Hello,", name)
+
+# favorite_song = input("What is your favorite song?")
+# print("Your favorite song is,", favorite_song)
+
+############################################################################################
+
+name = input("What is your name?")
+print("Hello,", name)
+
+sales = float(input("How much have you sold this month?"))
+
+sales = (sales * 13 / 100)
+print("You are entitled to" , sales)
